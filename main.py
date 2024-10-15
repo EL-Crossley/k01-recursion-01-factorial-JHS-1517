85084a78ce988dffa45c477a60a5d50a57b7bec7
Andrew <282hongshin@gmail.com>
2024-10-15 13:54:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-3# Put your function here
-
-
+# Put your function here
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    elif n>0:
+        return n * factorial(n-1)
+    else:
+        return print("invalidnumber")
+        
+   
+    
 # testing
-num = 5
+num = int(input())
 print(factorial(num))
